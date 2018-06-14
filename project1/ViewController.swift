9d183320fc757907044b319bf9c0bab90d9e0dd2
reeeeo <reo.117.pri@gmail.com>
2018-06-14 15:29:50 +0800

    add library 'SnapKit'

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -1,25 +1,38 @@
-//
-//  ViewController.swift
-//  project1
-//
-//  Created by okumura reo on 2018/06/13.
-//  Copyright © 2018年 reo. All rights reserved.
-//
-
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+  private lazy var container: UIView = {
+    let label = UITextField()
+    let container = UIView()
+    container.addSubview(label)
+    
+    label.text = ""
+    label.isEnabled = true
+    label.backgroundColor = .lightGray
+    label.snp.makeConstraints { make in
+      make.width.width.equalTo(UIScreen.main.bounds.width)
+      make.height.equalTo(40)
+      make.bottom.equalTo(container)
+    }
+    return container
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.addSubview(container)
+    container.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
 }
 
+//func onTappedPush(_ sender: UIButton) {
+//  print(sender)
+//  let vc = SecondViewController(titleName: "second")
+//  navigationController?.pushViewController(vc, animated: true)
+//}
